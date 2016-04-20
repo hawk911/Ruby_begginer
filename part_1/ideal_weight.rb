b9381ce6254e1e866e11bb8ae01ba7ece8216fb1
Denis B <hawk911@yandex.ru>
2016-04-20 13:32:17 +0300

    первая часть исправленна - забыл отступы

--- part_1/ideal_weight.rb
+++ part_1/ideal_weight.rb
@@ -5,7 +5,7 @@ puts "Какого ты роста?"
 height = gets.chomp.to_i
 ideal_weight = height - 110
 if ideal_weight>0
-puts "#{name}, твой идеальный вес равен #{ideal_weight}"	
+	puts "#{name}, твой идеальный вес равен #{ideal_weight}"	
 else
-puts "Ваш вес уже оптимальный!"	
+	puts "Ваш вес уже оптимальный!"	
 end
