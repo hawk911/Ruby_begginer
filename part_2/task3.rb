5618c517d43131a90865e597f430e7c9ed06eeb8
Denis B <hawk911@yandex.ru>
2016-04-25 19:44:23 +0300

    Изменение алгоритма Фибоначи

--- part_2/task3.rb
+++ part_2/task3.rb
@@ -1,11 +1,15 @@
 num = 100
 array = [1,1]
+=begin
 i=2
 
 until i > num
-      array[i] = array[i-1] + array[i-2]
-      array << array[i]
-      i +=1
+  array << array[i-1] + array[i-2]
+  i +=1
+end
+=end
+while array.length <=100
+  array << array[-1] + array [-2]
 end
 
 puts array
